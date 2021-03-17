35769642bde2387f0dc5085a51623b0fd515b64d
IlliaKhorunzhyi <80815379+IlliaKhorunzhyi@users.noreply.github.com>
2021-03-17 13:31:01 +0200

    Update and rename androBot2.js to androBot4.js

--- androBot4.js
+++ androBot4.js
@@ -14,8 +14,6 @@ function androAiChatBot(
             option_name_one,
             option_name_two
         ) {
-        
-        sleep(5000);
                 
             document.body.innerHTML += `
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
